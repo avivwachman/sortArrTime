61aeb7c7376ad57ab12142aa26a1955d09dcd6d2
avivwachman <68813905+avivwachman@users.noreply.github.com>
2020-08-12 17:55:15 +0300

    Add files via upload

---
+++ sortArrTime.java
@@ -0,0 +1,45 @@
+package com.wachman;
+import java.util.Stack;
+
+public class sortArrTime {
+    public static void main(String[] args) {
+
+    int[] arr =  {7,3,9,2};
+        sortArr(arr);
+    }
+
+    public static void sortArr(int[] arr){
+        Stack<Integer> copy = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            int finalI = i;
+            setTimeout(() -> toStack(arr[finalI], copy), arr[i]*100);
+        }
+        setTimeout(() -> printSort(copy,arr), arr[1]*400);
+    }
+
+    public static void toStack(int print,Stack<Integer> copy){
+        copy.push(print);
+    }
+
+    public static void printSort (Stack<Integer> copy, int[] arr){
+        for (int i = arr.length-1; i >=0 ; i--) {
+            arr[i]=copy.pop();
+        }
+    for (int j : arr) {
+      System.out.println(j);
+        }
+    }
+
+    public static void setTimeout(Runnable runnable, int delay){
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }).start();
+    }
+
+}
